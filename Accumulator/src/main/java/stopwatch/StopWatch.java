8ede2bcc62aa9e61c28b59f96efec927c98d2636
AnasElwkel <anshesh039@gmail.com>
2025-08-11 21:19:50 +0300

    StopWatch Green&Refactoring Phase 1

--- Accumulator/src/main/java/stopwatch/StopWatch.java
+++ Accumulator/src/main/java/stopwatch/StopWatch.java
@@ -2,7 +2,8 @@ package stopwatch;
 
 public class StopWatch {
     int minutes;
-    public void record(int minutes) { // Incorrect Logic -> doesn't record the minutes
+    public void record(int minutes) {
+        this.minutes = minutes;
     }
 
     public int getMinutes() {
